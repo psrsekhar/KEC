3506989986837c066ef5eaab76c9e58e620576bf
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-10-08 14:33:29 +0530

    added Arrow function

--- functions.js
+++ functions.js
@@ -15,12 +15,18 @@ function product(a, b) {
   return a * b;
 }
 
+let c = 2 + 3;
+
 //Function Expression - ES6
 const add = function (a, b) {
   return a + b;
 };
 
+//Arrow Functions - ES6
+const division = (a, b) => a / b;
+
 printName();
 printActressName("Trisha");
 console.log(product(3, 7));
 console.log(add(3, 7));
+console.log(division(9, 3));
