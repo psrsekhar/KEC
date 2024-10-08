ee567078889da86d13481ff0e5fb8045e8dcafd2
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-10-08 09:33:33 +0530

    change to external js

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Welcome to javascript!"); //for developers
+alert("About to execute javascript code...."); //for alerting warning messages
+console.log(prompt("enter your name: ")); //for dynamic user input
