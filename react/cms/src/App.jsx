52f652c4b5f3c6cdd624412208c78c4616912b87
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-10-09 08:31:25 +0530

    created a react project with vite

---
+++ react/cms/src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <header>Hi</header>
+      <main>Hello</main>
+      <footer>How are you?</footer>
+    </Fragment>
+  );
+}
+
+export default App;
